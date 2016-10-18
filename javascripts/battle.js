742856df6b1970a174fa7cf8671bb498f11365e7
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2016-10-18 17:47:55 -0500

    added more images and gifs

--- javascripts/battle.js
+++ javascripts/battle.js
@@ -7,13 +7,16 @@ var Gauntlet = (function(OldGauntlet) {
 		$("#deathEaterStats").append(`${deathEaterObj.playerName}<br />${deathEaterObj.house}<br />${deathEaterObj.spell} <br />${deathEaterObj.health}<br />`);
 		$("#img_player").html(studentObj.house.image);
 		$("#img_enemy").html(deathEaterObj.image);
-		console.log("deathEaterObj.image ",deathEaterObj.image);
 	};
 	OldGauntlet.generateImage = function(){
 		var Gifs = new Array();
 		Gifs[0]="../gif/gif1.gif";
 		Gifs[1]="../gif/gif2.gif";
 		Gifs[2]="../gif/gif3.gif";
+		Gifs[3]="../gif/gif4.gif";
+		Gifs[4]="../gif/gif5.gif";
+		Gifs[5]="../gif/gif6.gif";
+		Gifs[6]="../gif/gif7.gif";
 		var random = Math.round(Math.random() * Gifs.length - 1);
 		
 		var printGif = Gifs[random];
@@ -26,14 +29,11 @@ var Gauntlet = (function(OldGauntlet) {
 	};
 	OldGauntlet.runAttack = function(studentObj, deathEaterObj, studentType, battleObj){
         studentObj.health -= deathEaterObj.spell.damage;
-				var studentHealthBar = $("#student_health");
-				console.log("studentHealthBar.value", studentHealthBar);
-				studentHealthBar.attr('value', studentObj.health);
-        		console.log("student health, post damage: ",studentObj.health);
-			deathEaterObj.health -= studentType.damage;
-				var deathEaterHealthBar = $("#deatheater_health");
-        		deathEaterHealthBar.attr('value', deathEaterObj.health);
-        		console.log("deathEater health, post damage: ",deathEaterObj.health);
+		var studentHealthBar = $("#student_health");
+		studentHealthBar.attr('value', studentObj.health);
+		deathEaterObj.health -= studentType.damage;
+		var deathEaterHealthBar = $("#deatheater_health");
+        deathEaterHealthBar.attr('value', deathEaterObj.health);
         $("#battleStats").html("");		
 		$("#battleStats").html(`<div> <h1>Battle ${battleObj}</h1> ${studentObj.playerName} has ${studentType}</div>`);
         $("#battleStats").append(`<div>${deathEaterObj.playerName} has ${deathEaterObj.spell}</div>`);
